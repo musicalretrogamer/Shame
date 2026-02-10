78f0cd478afec5adbd6619427d041c18e4ee04a9
musicalretrogamer <musicalretrogamer@gmail.com>
2026-02-10 15:17:31 -0500

    Shame 1

---
+++ I don't know/Assets/Scripts/Respawnw.cs
@@ -0,0 +1,21 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Respawnw : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject prefab;
+    void Start()
+    {
+        for (var 1 = 0; isActiveAndEnabled < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(isA);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ I don't know/Assets/Scripts/wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter(Collider other)
+        {
+            Destroy(other.gameObject);
+
+        }
+}
+
+
